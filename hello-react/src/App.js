2c44cc8d2d00bb61c985096396bb23ad7911727d
dlsxjzld <sinjuk1@naver.com>
2022-02-11 12:30:04 +0900

    new dir

---
+++ hello-react/src/App.js
@@ -0,0 +1,13 @@
+import React, { Fragment } from "react";
+
+function App() {
+  const name = "리액트";
+  return (
+    <Fragment>
+      <h1>{name} 안녕!</h1>
+      <h2>잘 작동하니?</h2>
+    </Fragment>
+  );
+}
+
+export default App;
