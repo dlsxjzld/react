0fb7494bceb16520fa03c07d59ba4596a1993845
dlsxjzld <sinjuk1@naver.com>
2022-02-11 13:05:48 +0900

    tutorial

--- hello-react/src/App.js
+++ hello-react/src/App.js
@@ -1,12 +1,16 @@
 import React, { Fragment } from "react";
+import "./App.css";
 
 function App() {
   const name = "리액트";
   return (
-    <Fragment>
-      <h1>{name} 안녕!</h1>
-      <h2>잘 작동하니?</h2>
-    </Fragment>
+    <div className="react">
+      {/* if문 대신 조건부 연산자 */}
+      {/* {name === "리액트" ? <h1>리액트입니다.</h1> : <h2>리액트가 아닙니다.</h2>} */}
+      {/* AND 연산자를 사용한 조건부 렌더링 */}
+      {/* {name === "리액트" && <h1>리액트입니다.</h1>} */}
+      {name}
+    </div>
   );
 }
 
