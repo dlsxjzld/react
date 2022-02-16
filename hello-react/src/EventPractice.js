ada14b2733d752758b74a69e2cb8974dc4ebfd30
dlsxjzld <sinjuk1@naver.com>
2022-02-16 13:54:12 +0900

    함수형 컴포넌트로 구현

--- hello-react/src/EventPractice.js
+++ hello-react/src/EventPractice.js
@@ -2,15 +2,28 @@ import React, { useState } from "react";
 
 /// 인풋이 적어 onChange 함수를 따로 만들었다.
 /// 많아진다면 e.target.name 을 이용하는게 낫다.
+/// e.target.name을 활용하려면 useState를 쓸 때
+/// 인풋 값들이 들어 있는 form 객체를 사용
 const EventPractice = () => {
-  const [username, setUsername] = useState("");
-  const [message, setMessage] = useState("");
-  const onChangeUsername = (e) => setUsername(e.target.value);
-  const onChangeMessage = (e) => setMessage(e.target.value);
+  const [form, setForm] = useState({
+    username: "",
+    message: "",
+  });
+  const { username, message } = form; // 비구조화 할당
+  const onChange = (e) => {
+    const nextForm = {
+      ...form, // 기존의 form 내용을 이 자리에 복사한 뒤
+      [e.target.name]: e.target.value, //원하는 값을 덮어 씌우기
+    };
+    setForm(nextForm);
+  };
+
   const onClick = () => {
     alert(username + ": " + message);
-    setUsername("");
-    setMessage("");
+    setForm({
+      username: "",
+      message: "",
+    });
   };
   const onKeyPress = (e) => {
     if (e.key === "Enter") {
@@ -26,14 +39,14 @@ const EventPractice = () => {
         name="username"
         placeholder="사용자명"
         value={username}
-        onChange={onChangeUsername}
+        onChange={onChange}
       ></input>
       <input
         type="text"
         name="message"
         placeholder="아무거나 입력해 보세요"
         value={message}
-        onChange={onChangeMessage}
+        onChange={onChange}
         onKeyPress={onKeyPress}
       ></input>
       <button onClick={onClick}>확인</button>
