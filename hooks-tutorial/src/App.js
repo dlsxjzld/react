66431412a2ac96bba094ec940bf4469b798defb6
dlsxjzld <sinjuk1@naver.com>
2022-02-20 18:02:53 +0900

    useMemo

--- hooks-tutorial/src/App.js
+++ hooks-tutorial/src/App.js
@@ -1,6 +1,7 @@
 import React, { useState } from "react";
 import Counter from "./Counter";
 import Info from "./Info";
+import Average from "./Average";
 
 const App = () => {
   const [visible, setVisible] = useState(false);
@@ -8,7 +9,7 @@ const App = () => {
   return (
     <div>
       {/* <Counter></Counter> */}
-      <button
+      {/* <button
         onClick={() => {
           setVisible(!visible);
         }}
@@ -16,7 +17,8 @@ const App = () => {
         {visible ? "숨기기" : "보이기"}
       </button>
       <hr />
-      {visible && <Info></Info>}
+      {visible && <Info></Info>} */}
+      <Average></Average>
     </div>
   );
 };
