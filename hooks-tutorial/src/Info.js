827cc7ac1c6a1c99fc9e7b11901c94f932b7dd17
dlsxjzld <sinjuk1@naver.com>
2022-02-20 17:12:30 +0900

    useReducer

--- hooks-tutorial/src/Info.js
+++ hooks-tutorial/src/Info.js
@@ -1,12 +1,19 @@
-import React, { useState, useEffect } from "react";
+import React, { useState, useEffect, useReducer } from "react";
+
+function reducer(state, action) {
+  return {
+    ...state,
+    [action.name]: action.value,
+  };
+}
 
 //useEffect 는 리액트 컴포넌트가 렌더링될 때마다
 //특정 작업을 수행하도록 설정할 수 있는 Hook
 // 클래스형 컴포넌트의 componentDidMount와
 // componentDidUpdate를 합친형태로 봐도 무방하다
 const Info = () => {
-  const [name, setName] = useState("");
-  const [nickname, setNickname] = useState("");
+  // const [name, setName] = useState("");
+  // const [nickname, setNickname] = useState("");
 
   // useEffect(() => {
   //   console.log("렌더링이 완료되었습니다.");
@@ -24,30 +31,40 @@ const Info = () => {
   //   console.log(name);
   // }, [name]);
 
-  useEffect(() => {
-    // 컴포넌트가 언마운트되기 전이나 업데이트되기 직전에
-    // 어떠한 작업을 수행하고 싶다면 cleanup함수를 반환해야한다
-    console.log("effect");
-    console.log(name);
-    return () => {
-      console.log("cleanup");
-      console.log(name);
-    };
-  }, [name]);
+  // useEffect(() => {
+  //   // 컴포넌트가 언마운트되기 전이나 업데이트되기 직전에
+  //   // 어떠한 작업을 수행하고 싶다면 cleanup함수를 반환해야한다
+  //   console.log("effect");
+  //   console.log(name);
+  //   return () => {
+  //     console.log("cleanup");
+  //     console.log(name);
+  //   };
+  // }, [name]);
 
-  const onChangeName = (e) => {
-    setName(e.target.value);
-  };
+  // const onChangeName = (e) => {
+  //   setName(e.target.value);
+  // };
 
-  const onChangeNickname = (e) => {
-    setNickname(e.target.value);
+  // const onChangeNickname = (e) => {
+  //   setNickname(e.target.value);
+  // };
+
+  const [state, dispatch] = useReducer(reducer, {
+    //8.3 useReducer
+    name: "",
+    nickname: "",
+  });
+  const { name, nickname } = state;
+  const onChange = (e) => {
+    dispatch(e.target);
   };
 
   return (
     <div>
       <div>
-        <input value={name} onChange={onChangeName}></input>
-        <input value={nickname} onChange={onChangeNickname}></input>
+        <input name="name" value={name} onChange={onChange}></input>
+        <input name="nickname" value={nickname} onChange={onChange}></input>
       </div>
       <div>
         <div>
