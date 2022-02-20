bb46980b97ded37558c4387942edc402c291e7c0
dlsxjzld <sinjuk1@naver.com>
2022-02-20 15:53:37 +0900

    useState

---
+++ hooks-tutorial/src/App.js
@@ -0,0 +1,14 @@
+import React from "react";
+import Counter from "./Counter";
+import Info from "./Info";
+
+const App = () => {
+  return (
+    <div>
+      {/* <Counter></Counter> */}
+      <Info></Info>
+    </div>
+  );
+};
+
+export default App;
