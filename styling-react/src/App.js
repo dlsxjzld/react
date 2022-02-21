572d1a9c2be2382fa0be4bac6a46f948fb16ce3a
dlsxjzld <sinjuk1@naver.com>
2022-02-21 16:37:51 +0900

    CSS Module

--- styling-react/src/App.js
+++ styling-react/src/App.js
@@ -2,12 +2,14 @@ import React, { Component } from "react";
 import logo from "./logo.svg";
 import "./App.css";
 import SassComponent from "./SassComponent";
+import CSSModule from "./CSSModule";
 
 class App extends Component {
   render() {
     return (
       <div>
-        <SassComponent></SassComponent>
+        {/* <SassComponent></SassComponent> */}
+        <CSSModule />
       </div>
     );
   }
