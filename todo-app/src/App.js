c7dcebf4dcb543ed89bd02222f176d220fe34f4a
dlsxjzld <sinjuk1@naver.com>
2022-02-24 16:13:59 +0900

    ch11_5_2 useReducer 사용하기

--- todo-app/src/App.js
+++ todo-app/src/App.js
@@ -1,4 +1,4 @@
-import React, { useState, useRef, useCallback } from 'react';
+import React, { useReducer, useRef, useCallback } from 'react';
 import TodoTemplate from './components/TodoTemplate';
 import TodoInsert from './components/TodoInsert';
 import TodoList from './components/TodoList';
@@ -21,8 +21,26 @@ function createBultTodos() {
   return array;
 }
 
+function todoReducer(todos, action) {
+  switch (action.type) {
+    case 'INSERT': //새로 추가
+      //{type: 'INSERT' , todo : {id : 1, text:'todo', checked:false}}
+      return todos.concat(action.todo);
+    case 'REMOVE': // 제거
+      //{type:'REMOVE', id:1}
+      return todos.filter((todo) => todo.id !== action.id);
+    case 'TOGGLE': // 토글
+      // {type:'TOGGLE', id:1}
+      return todos.map((todo) =>
+        todo.id === action.id ? { ...todo, checked: !todo.checked } : todo,
+      );
+    default:
+      return todos;
+  }
+}
+
 const App = () => {
-  const [todos, setTodos] = useState(createBultTodos);
+  const [todos, dispatch] = useReducer(todoReducer, undefined, createBultTodos);
 
   //고윳값으로 사용될 id
   // ref를 사용하여 변수 담기
@@ -35,8 +53,7 @@ const App = () => {
         text,
         checked: false,
       };
-      // setTodos(todos.concat(todo));
-      setTodos((todos) => todos.concat(todo)); // useState의 함수형 업데이트
+      dispatch({ type: 'INSERT', todo });
       nextId.current += 1; // nextId 1씩 더하기
     },
     // [todos],
@@ -45,8 +62,7 @@ const App = () => {
 
   const onRemove = useCallback(
     (id) => {
-      // setTodos(todos.filter((todo) => todo.id !== id));
-      setTodos((todos) => todos.filter((todo) => todo.id !== id));
+      dispatch({ type: 'REMOVE', id });
     },
     // [todos],
     [],
@@ -54,12 +70,7 @@ const App = () => {
 
   const onToggle = useCallback(
     (id) => {
-      // setTodos(
-      setTodos((todos) =>
-        todos.map((todo) =>
-          todo.id === id ? { ...todo, checked: !todo.checked } : todo,
-        ),
-      );
+      dispatch({ type: 'TOGGLE', id });
     },
     // [todos],
     [],
