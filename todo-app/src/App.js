176c9da32203e4184b236507e3fdb07344d27c2f
dlsxjzld <sinjuk1@naver.com>
2022-02-24 16:06:00 +0900

    ch11_5 useState의 함수형 업데이트

--- todo-app/src/App.js
+++ todo-app/src/App.js
@@ -3,28 +3,30 @@ import TodoTemplate from './components/TodoTemplate';
 import TodoInsert from './components/TodoInsert';
 import TodoList from './components/TodoList';
 
-const App = () => {
-  const [todos, setTodos] = useState([
-    {
-      id: 1,
-      text: '리액트의 기초 알아보기',
-      checked: true,
-    },
-    {
-      id: 2,
-      text: '컴포넌트 스타일링해 보기',
-      checked: true,
-    },
-    {
-      id: 3,
-      text: '일정 관리 앱 만들어 보기',
+// useState, useRef, useCallback
+// 배열 map , filter , concat
+// 객체 ...~ (spread)
+// scss classnames ( 조건부 스타일링 )
+// 객체 비구조화 할당
+
+function createBultTodos() {
+  const array = [];
+  for (let i = 1; i <= 2500; i++) {
+    array.push({
+      id: i,
+      text: `할 일 ${i}`,
       checked: false,
-    },
-  ]);
+    });
+  }
+  return array;
+}
+
+const App = () => {
+  const [todos, setTodos] = useState(createBultTodos);
 
   //고윳값으로 사용될 id
   // ref를 사용하여 변수 담기
-  const nextId = useRef(4);
+  const nextId = useRef(2501);
 
   const onInsert = useCallback(
     (text) => {
@@ -33,28 +35,34 @@ const App = () => {
         text,
         checked: false,
       };
-      setTodos(todos.concat(todo));
+      // setTodos(todos.concat(todo));
+      setTodos((todos) => todos.concat(todo)); // useState의 함수형 업데이트
       nextId.current += 1; // nextId 1씩 더하기
     },
-    [todos],
+    // [todos],
+    [],
   );
 
   const onRemove = useCallback(
     (id) => {
-      setTodos(todos.filter((todo) => todo.id !== id));
+      // setTodos(todos.filter((todo) => todo.id !== id));
+      setTodos((todos) => todos.filter((todo) => todo.id !== id));
     },
-    [todos],
+    // [todos],
+    [],
   );
 
   const onToggle = useCallback(
     (id) => {
-      setTodos(
+      // setTodos(
+      setTodos((todos) =>
         todos.map((todo) =>
           todo.id === id ? { ...todo, checked: !todo.checked } : todo,
         ),
       );
     },
-    [todos],
+    // [todos],
+    [],
   );
 
   return (
