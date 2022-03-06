93c69b0e7f2652c06f80281d0ff5585d72788110
dlsxjzld <sinjuk1@naver.com>
2022-03-06 14:46:01 +0900

    중첩된 라우트_1_공통 레이아웃 컴포넌트

--- router-tutorial/src/App.js
+++ router-tutorial/src/App.js
@@ -3,14 +3,23 @@ import { Route, Routes, Link } from "react-router-dom";
 import About from "./About";
 import Home from "./Home";
 import Profile from "./Profile";
+import Article from "./Article";
+import Articles from "./Articles";
+import Layout from "./Layout";
 
 const App = () => {
   return (
     <div>
       <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/about" element={<About />} />
-        <Route path="/profiles/:username" element={<Profile />} />
+        <Route element={<Layout />}>
+          <Route path="/" element={<Home />} />
+          <Route path="/about" element={<About />} />
+          <Route path="/profiles/:username" element={<Profile />} />
+        </Route>
+
+        <Route path="/articles" element={<Articles />}>
+          <Route path=":id" element={<Article />} />
+        </Route>
       </Routes>
     </div>
   );
