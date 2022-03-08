52e94d35031b53cf27db42d58fc2b25f1637091f
dlsxjzld <sinjuk1@naver.com>
2022-03-08 15:58:31 +0900

    ch14.3~14.6

--- news-viewer/src/App.js
+++ news-viewer/src/App.js
@@ -1,39 +1,14 @@
-import React, { useState } from "react";
-import axios from "axios";
+import React, { useState, useCallback } from "react";
+import Categories from "./components/Categories";
+import NewsList from "./components/NewsList";
 
 const App = () => {
-  const [data, setData] = useState(null);
-  // const onClick = () => {
-  //   axios
-  //     .get("https://jsonplaceholder.typicode.com/todos/1")
-  //     .then((response) => {
-  //       setData(response.data);
-  //     });
-  // };
-
-  const onClick = async () => {
-    try {
-      const response = await axios.get(
-        "https://jsonplaceholder.typicode.com/todos/1"
-      );
-      setData(response.data);
-    } catch (e) {
-      console.log(e);
-    }
-  };
-
+  const [category, setCategory] = useState("all");
+  const onSelect = useCallback((category) => setCategory(category), []);
   return (
     <div>
-      <div>
-        <button onClick={onClick}>불러오기</button>
-      </div>
-      {data && (
-        <textarea
-          rows={7}
-          value={JSON.stringify(data, null, 2)}
-          readOnly={true}
-        />
-      )}
+      <Categories category={category} onSelect={onSelect}></Categories>
+      <NewsList category={category}></NewsList>
     </div>
   );
 };
