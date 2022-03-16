0c84561cff4fb2a83b4798af9e2e419ce6c57f66
dlsxjzld <sinjuk1@naver.com>
2022-03-16 12:33:08 +0900

    ch18.3.2 redux-saga

--- learn-redux-middleware/src/index.js
+++ learn-redux-middleware/src/index.js
@@ -6,16 +6,19 @@ import reportWebVitals from "./reportWebVitals";
 import { applyMiddleware, createStore } from "redux";
 import { Provider } from "react-redux";
 import { composeWithDevTools } from "redux-devtools-extension";
-import rootReducer from "./modules/index";
+import rootReducer, { rootSaga } from "./modules/index";
 // import loggerMiddleware from "./lib/loggerMiddleware";
 import { createLogger } from "redux-logger";
 import ReduxThunk from "redux-thunk";
+import createSagaMiddleware from "@redux-saga/core";
 
 const logger = createLogger();
+const sagaMiddleware = createSagaMiddleware();
 const store = createStore(
   rootReducer,
-  composeWithDevTools(applyMiddleware(logger, ReduxThunk))
+  composeWithDevTools(applyMiddleware(logger, ReduxThunk, sagaMiddleware))
 );
+sagaMiddleware.run(rootSaga);
 ReactDOM.render(
   <Provider store={store}>
     <App />
