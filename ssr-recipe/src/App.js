b4cc68ed59878b2b0b0ae7ac7a9abc0e672d261b
dlsxjzld <sinjuk1@naver.com>
2022-03-16 14:10:15 +0900

    Commit before eject

---
+++ ssr-recipe/src/App.js
@@ -0,0 +1,20 @@
+import React from "react";
+import { Route, Routes } from "react-router-dom";
+import Menu from "./components/Menu";
+import RedPage from "./pages/RedPage";
+import BluePage from "./pages/BluePage";
+
+const App = () => {
+  return (
+    <div>
+      <Menu />
+      <hr />
+      <Routes>
+        <Route path="/red" element={<RedPage />}></Route>
+        <Route path="/blue" element={<BluePage />}></Route>
+      </Routes>
+    </div>
+  );
+};
+
+export default App;
